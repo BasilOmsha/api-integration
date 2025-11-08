553a5eece19e81c2ebb77ed8916946776619f974
Basel <basil.omsha@gmail.com>
2025-11-08 14:52:56 +0200

    simplifying dtos

--- api-integration.Application/src/FactoryInterfaces/Fingrid/IApiMapperFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace api_integration.Application.src.FactoryInterfaces.Fingrid
-{
-    public interface IApiMapperFactory<in TApiResponse, out TReadDto>
-    {
-        TReadDto MapFromApiResponse(TApiResponse apiResponse);
-    }
-}
